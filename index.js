a9a1518ad7a15dd618cf41399258077740363771
Chrissy Savinell <csavinell@yahoo.com>
2021-01-27 22:08:57 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function updateObjectWithKeyAndValue(obj, key, value) {
+  return Object.assign({}, obj, { [key]: value })
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+
+  return obj
+}
+
+function deleteFromObjectByKey(obj, key) {
+  var oldObj = {obj: 'key'}
+  var newObj = Object.assign({}, oldObj)
+
+  delete newObj.key
+  return newObj;
+}
+function  destructivelyDeleteFromObjectByKey(object, key) {
+  var oldObj = {object: 'key'}
+  delete oldObj.key;
+    return object;
+}
